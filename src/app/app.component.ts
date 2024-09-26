991faa57ae811cf0ce6c8982aa182e2a052b4e25
Kevin <eslick.kevin@yahoo.com>
2024-09-26 11:14:17 -0500

    first angular commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './modules/_login/_dashboard/_navbar/nav-bar.component';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'first-angular-app';
---
+++ src/app/modules/_login/_dashboard/_navbar/nav-bar.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css', // Complete relative path
+})
+export class NavbarComponent {}
